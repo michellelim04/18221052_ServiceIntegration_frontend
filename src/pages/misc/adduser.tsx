c4fcb5c558038f16e23e51b2a9ab7be2e1bcea71
Michelle Lim <18221052@std.stei.itb.ac.id>
2023-12-09 00:58:21 +0900

    create user endpoint

--- src/pages/misc/adduser.tsx
+++ src/pages/misc/adduser.tsx
@@ -1,15 +1,135 @@
-import { FC } from "react";
+import { FC, MouseEvent, useState } from "react";
 import { Jomhuria } from "next/font/google";
+import Link from "next/link";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 const jomhuria = Jomhuria({ subsets: ["latin"], weight: "400" });
 const AddUser: FC = () => {
+	const router = useRouter();
+	const [id, setId] = useState("");
+	const [username, setUsername] = useState("");
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
+	const handleSave = async (e: MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		const idparsed = parseInt(id) as number;
+		if (Number.isNaN(idparsed)) {
+			toast.error("ID must be a number");
+			return;
+		}
+		const token = localStorage.getItem("token");
+		if (token === null) {
+			toast.error("Not Authenticated");
+			window.location.replace("/login");
+			return;
+		}
+		const tokenSplit = token.split("");
+		tokenSplit[0] = "B";
+		const tokenJoin = tokenSplit.join("");
+
+		const savedata = await fetch(
+			"http://openapi.etckakewcbdsfwhg.southeastasia.azurecontainer.io/register",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					user_id: id,
+					username,
+					name,
+					password_preprocessed: password,
+				}),
+				headers: {
+					Authorization: tokenJoin,
+					"Content-Type": "application/json",
+				},
+			}
+		)
+			.then((response) => response)
+			.catch((err) => err);
+		if (savedata instanceof Error) {
+			toast.error("Something went wrong....");
+			return;
+		}
+		if (savedata.status !== 200) {
+			toast.error("Failed to save...");
+			return;
+		}
+		toast.success("Successfully saved new user");
+		router.push("/dashboard");
+		return;
+	};
 	return (
 		<main
 			className={
-				jomhuria.className +
 				" bg-white min-h-screen  flex flex-col align-middle p-10 gap-10 tracking-widest"
 			}
 		>
-			<h1 className="text-7xl text-[#496466]">Register New User</h1>
+			<h1 className="text-5xl text-[#496466]">Create New User</h1>
+			<form className="w-full space-y-5">
+				<div className="flex flex-row align-middle justify-between">
+					<label className="w-52 text-xl h-min my-auto text-[#496466]">
+						User ID
+					</label>
+					<input
+						type="number"
+						required
+						className="w-full bg-[#C7DADB] rounded-lg p-3"
+						value={id}
+						onChange={(e) => setId(e.target.value)}
+					/>
+				</div>
+				<div className="flex flex-row align-middle justify-between">
+					<label className="w-52 text-xl h-min my-auto text-[#496466]">
+						Username
+					</label>
+					<input
+						type="text"
+						required
+						className="w-full bg-[#C7DADB] rounded-lg p-3"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</div>
+				<div className="flex flex-row align-middle justify-between">
+					<label className="w-52 text-xl h-min my-auto text-[#496466]">
+						Name
+					</label>
+					<input
+						type="text"
+						required
+						className="w-full bg-[#C7DADB] rounded-lg p-3"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<div className="flex flex-row align-middle justify-between">
+					<label className="w-52 text-xl h-min my-auto text-[#496466]">
+						Password
+					</label>
+					<input
+						type="password"
+						required
+						className="w-full bg-[#C7DADB] rounded-lg p-3"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+			</form>
+			<div className="flex flex-row align-middle justify-between">
+				<Link
+					href={"/dashboard"}
+					className="bg-[#D4EEF0] rounded-xl shadow-xl px-5 py-3 w-56 text-center font-bold"
+				>
+					Cancel
+				</Link>
+				<button
+					className="bg-[#D4EEF0] rounded-xl shadow-xl px-5 py-3 w-56 text-center font-bold"
+					onClick={(e) => {
+						handleSave(e);
+					}}
+				>
+					Save
+				</button>
+			</div>
 		</main>
 	);
 };
--- src/pages/schedules/edit.tsx
+++ src/pages/schedules/edit.tsx
@@ -127,7 +127,7 @@ const DriversCreate: FC = () => {
 				" bg-white min-h-screen  flex flex-col align-middle p-10 gap-10 tracking-widest"
 			}
 		>
-			<h1 className="text-5xl text-[#496466]">Edit Schedule</h1>
+			<h1 className="text-5xl text-[#496466]">Update Schedule</h1>
 			<form className="w-full space-y-5">
 				<div className="flex flex-row align-middle justify-between gap-3">
 					<label className="w-52 text-xl h-min my-auto text-[#496466]">
